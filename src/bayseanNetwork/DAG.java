e8385cd58dce9400f8f2f5eea4229fb0ee96c963
imran353 <imran.mamade@ist.utl.pt>
2015-05-15 03:39:46 +0100

    add() done

--- src/bayseanNetwork/DAG.java
+++ src/bayseanNetwork/DAG.java
@@ -4,18 +4,22 @@ import java.util.Arrays;
 
 public class DAG {
 	//Matriz de Adjacências e respectivos métodos
-	int[][] dag;
+	public boolean [][] dag;
 	DataSet data_set;
 	
 	public DAG(DataSet data_set){
-		dag = new int[data_set.num_var*2][data_set.num_var];
+		dag = new boolean[data_set.num_var*2][data_set.num_var];
+		
+		for(int i=data_set.num_var; i<(2*data_set.num_var); i++){
+			dag[i][i-data_set.num_var]=false;
+		}
 		this.data_set = data_set;
 		
 		//For test purpose
-		dag[0][1] = 1;
-		dag[1][2] = 1;
-		dag[2][2] = 1;
-		dag[3][1] = 1;
+		//dag[0][1] = true;
+		//dag[1][2] = true;
+		//dag[2][2] = true;
+		//dag[3][1] = true;
 		//To delete after implementation
 	}
 	
@@ -29,40 +33,59 @@ public class DAG {
 	}
 
 
-	void add(){ //Tem que gerar uma excepção quando não é possível
+	public void add(int origem, int destino){ //Tem que gerar uma excepção quando não é possível
+		
+		if(this.dag[origem][convertDestination(destino)]==true){
+			System.out.println("Vector ja adicionado!");
+			return;
+		}
+		if(origem==destino){ 
+			System.out.println("origem é igual ao destino");
+			dag.toString();
+			return; /* nao pode meter true*/
+		}
+		if(origem<this.data_set.num_var){
+			System.out.println("Vector correctamente adicionado1");
+			this.dag[origem][convertDestination(destino)]=true;
+			return;
+		}
+		if (origem>=this.data_set.num_var){
+			if (dag[destino][origem-this.data_set.num_var]==true){
+				System.out.println("A aresta contrario já se encontra preenchida!");
+				return;
+			}
+		}else{
+			
+			if (dag[convertDestination(destino)][origem]==true){
+				System.out.println("A aresta contrario já se encontra preenchida!");
+				dag.toString();
+				return; /*nao pode meter true*/
+			}
+		}
+		
+		boolean[] visitedVector = new boolean[2*this.data_set.num_var];
+		for(int j=0;j<this.data_set.num_var;j++) visitedVector[j]=false;
+		if(DFS(origem,destino,visitedVector)){
+			System.out.println("Nao foi possivel adicionar vector");
+			dag.toString();
+		}else{
+			System.out.println("Vector correctamente adicionado");
+			System.out.println(origem + " " + destino);
+			this.dag[origem][convertDestination(destino)]=true;
+		}
+		
+		//perguntar se em vez de ligar de x->y ou y->x faz diferenca no score
+		//verifyDag();
 		
 	}
 	
-	void remove(){ //Tem que gerar uma excepção quando não é possível
+	void remove(int linha, int coluna){ //Tem que gerar uma excepção quando não é possível
 		
+		dag[linha][coluna]=false;
 	}
 	
 	void reverse(){ //Tem que gerar uma excepção quando não é possível
 		
-	}
-	
-	//Receives Parents Configuration and returns int with parent configuration
-	public int toParentConfiguration (int node, int[][] parent_configuration){
-		int j= 1; //empty configuration standart
-		
-		//Check if node is from t+1 (Nodes from t have empty parent configuration)
-		if (node < this.data_set.num_var) return j;
-		
-		int real_node = realNode(node);
-		int num_parents = numParents(real_node);
-		
-		//no parents means empty parent configuration
-		if (num_parents == 0) return j;
-		
-		int [][] ri_parents = riParents(real_node, num_parents);
-		
-		j = parent_configuration[0][1];
-		for (int i = 1; i < num_parents; i++) {
-			j = j * ri_parents[i][1];
-			j = j + parent_configuration[i][1];
-		}
-		System.out.println("configuration: " + j);
-		return j;
 	}
 	
 	//Receives Parent Configuration and returns array with parent node number and configuration
@@ -71,7 +94,8 @@ public class DAG {
 		
 		int[][] q;
 		
-		if (parent_configuration >= maxq(node)) throw new PCInvalid();
+		//Check if node is from t+1 (Nodes from t have empty parent configuration)
+		if (node < this.data_set.num_var) throw new NoParent();
 		
 		int real_node = realNode(node);
 		int num_parents = numParents(real_node);
@@ -79,13 +103,21 @@ public class DAG {
 		if (num_parents == 0) throw new NoParent();
 		
 		q = new int[num_parents][2];
+		
 		int [][] ri_parents = riParents(real_node, num_parents);
+		
+		int max_q = 1;
+		for (int i = 0; i < ri_parents.length; i++) {
+			max_q = max_q * ri_parents [i][1];
+		}
+		
+		if (parent_configuration >= max_q) throw new PCInvalid();
 		
 		int parent_configuration_aux = parent_configuration;
 		for (int i = ri_parents.length - 1; i >= 0 ; i--) {
 			q[i][0] = ri_parents[i][0];
 			q[i][1] = parent_configuration_aux % ri_parents[i][1];
-			parent_configuration_aux = (parent_configuration_aux - q [i][1])/ri_parents[i][1];
+			System.out.println("parent " + q [i][0] + ", q: " + q [i][1]);
 		}
 		return q;
 	}
@@ -99,7 +131,7 @@ public class DAG {
 	private int numParents(int real_node){
 		int num_parents = 0;
 		for (int i = 0; i < this.data_set.num_var*2 ; i++) {
-			if (dag[i][real_node] == 1){
+	/*ver*/		if (dag[i][real_node] == true){
 				num_parents ++;
 			}
 		}
@@ -112,7 +144,7 @@ public class DAG {
 		
 		int j = 0;
 		for (int i = 0; i < this.data_set.num_var*2 ; i++) {
-			if (dag[i][real_node] == 1){
+	/*ver*/		if (dag[i][real_node] == true){
 				ri_parents [j][0] = i;
 				if (i < data_set.num_var){
 					ri_parents [j][1] = this.data_set.ri[i];
@@ -124,24 +156,23 @@ public class DAG {
 		}
 		return ri_parents;
 	}
-
-
-	int maxq(int node) throws NoParent {
-		//Check if node is from t+1 (Nodes from t have empty parent configuration)
-		if (node < this.data_set.num_var) throw new NoParent();
-		
-		int real_node = realNode(node);
-		int num_parents = numParents(real_node);
-		
-		if (num_parents == 0) throw new NoParent();
-		
-		int [][] ri_parents = riParents(real_node, num_parents);
-		
-		int max_q = 1;
-		for (int i = 0; i < ri_parents.length; i++) {
-			max_q = max_q * ri_parents [i][1];
-		}
-		
-		return max_q;
+	
+	private int convertDestination(int destino){
+		destino = destino-data_set.num_var;
+		return destino;
 	}
+	
+	private boolean DFS(int origem, int destino,boolean[] visitedVector){
+		
+		for(int i=0; i<this.data_set.num_var; i++){
+			if(this.dag[destino][i]==true && visitedVector[i]==false){
+				if(i+this.data_set.num_var==origem) return true;
+				visitedVector[i+this.data_set.num_var]=true;
+				if(DFS(origem,i+this.data_set.num_var,visitedVector)) return true;
+			}
+			
+		}
+		return false;		
+	}
+	
 }
