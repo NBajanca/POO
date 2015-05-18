d9f10f37b335aad50782f9077d2a827cb4d3c853
Nuno Bajanca <nunombajanca@gmail.com>
2015-05-18 02:12:40 +0100

    BUG in MDL

--- src/bayseanNetwork/MDL.java
+++ src/bayseanNetwork/MDL.java
@@ -1,12 +1,16 @@
 package bayseanNetwork;
 
 public class MDL extends LL {
+	/* (non-Javadoc)
+	 * @see bayseanNetwork.LL#compute(bayseanNetwork.DAG)
+	 */
 	@Override
 	public double compute(DAG dag) {
 		double value, b = 0;
-		int N =  dag.data_set.num_var*2;
+		int N =  dag.data_set.data.size();
 		value =  super.compute(dag);
-		for (int i = 0; i < N; i++) {
+		
+		for (int i = 0; i < dag.data_set.num_var*2; i++) {
 			int q = 1;
 			try {
 				q = dag.maxq(i);
@@ -14,8 +18,7 @@ public class MDL extends LL {
 				q = 1;
 			}
 			b += (dag.data_set.ri[dag.realNode(i)]-1)*q;
-		}
-	
+		}	
 		
 		value -= 0.5*(Math.log(N)/Math.log(2))*Math.abs(b);
 		return value;
